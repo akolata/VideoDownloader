ed196e3d5bb9f82f1af845363ea8ba65416e78fe
Aleksander Kołata <olek.tbg.konta@gmail.com>
2017-04-17 20:52:10 +0200

    Main method cleaned

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import utils.Downloader;
+
+/**
+ * Created by Aleksander Kołata on 2017-04-15.
+ */
+public class Main {
+
+    public static void main(String[] args)  {
+
+        Downloader downloader = new Downloader();
+
+
+    }
+
+}

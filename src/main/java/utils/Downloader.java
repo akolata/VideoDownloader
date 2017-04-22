132e2ea57621382daf574fcd30bae1fed4b5418f
Aleksander Kolata <olek.tbg.konta@gmail.com>
2017-04-22 16:12:19 +0200

    Changes in Downloader made

--- src/main/java/utils/Downloader.java
+++ src/main/java/utils/Downloader.java
@@ -17,35 +17,79 @@ import java.util.logging.*;
  */
 public class Downloader {
 
-
     /**
-     * List of correct URLs to a videos
+     * List of videos URLs
      */
     public  List<URL>     videoURLs = new LinkedList<>();
     /**
      * Logger used
      */
-    private  final Logger LOG = Logger.getLogger(Downloader.class.getSimpleName());
+    private final Logger LOG = Logger.getLogger(Downloader.class.getSimpleName());
     /**
      * Template used to generate URL.
      */
-    private  final String URL_TEMPLATE = "";
+    public   String urlTemplate = "";
     /**
      * Output file template
      */
-    private  final String FILENAME_TEMPLATE = "";
+    public   String downloadedFileTemplate = "";
     /**
      * Location of output folder
      */
-    private  final String DOWNLOAD_FOLDER_LOCATION = "";
+    public   String downloadFolderLocation = "";
     /**
      * Variable used to count,how many videos were downloaded
      */
-    private  int          VIDEO_NUMBER = 0;
+    public  int currentlyDownloadingVideoNumber = 0;
     /**
      * How many video gonna be downloaded
      */
-    private  int          VIDEO_COUNT = 102;
+    public int numberOfAllVideos = 0;
+
+    /**
+     * Default constructor, with Logger configuration
+     */
+    public Downloader(){
+        LOG.setUseParentHandlers(false);
+        LOG.setLevel(Level.ALL);
+
+        Handler consoleHandler = new ConsoleHandler();
+        consoleHandler.setFormatter(new LogFormatter());
+
+        LOG.addHandler(consoleHandler);
+        LOG.info("Application started");
+    }
+
+    /**
+     * Used to add additional logger file
+     * @param logFileLocation path to file with log messages
+     */
+    public void setFileLogHandler(String logFileLocation){
+        Handler fileHandler;
+        try {
+            fileHandler = new FileHandler(logFileLocation,true);
+            fileHandler.setLevel(Level.ALL);
+            fileHandler.setFormatter(new LogFormatter());
+            LOG.addHandler(fileHandler);
+            LOG.info("FileHandler added. File with logs : " + logFileLocation);
+        } catch (IOException e) {
+            logException(e);
+        }
+    }
+
+    /**
+     * Shows info about thrown exception in formatted way
+     * @param exception thrown Exception
+     */
+    private void logException(Exception exception){
+        StringWriter sw = new StringWriter();
+        exception.printStackTrace(new PrintWriter(sw));
+        String stacktrace = sw.toString();
+        LOG.severe("An exception : " + exception.getClass().getSimpleName() + " occured");
+        LOG.severe("Caused by : " + exception.getCause());
+        LOG.severe("Exception message : " + exception.getMessage());
+        LOG.severe("Stack trace : \n" + stacktrace);
+    }
 
     /**
      * Reads all lines from given file's path - each line is video URL
@@ -55,67 +99,43 @@ public class Downloader {
         try {
             List<String> urlsAsStringsList = Files.readAllLines(Paths.get(path));
             videoURLs = getVideosURLsFromStringList(urlsAsStringsList);
+            numberOfAllVideos = videoURLs.size();
+            LOG.info(videoURLs.size() + " read from " + path + " file");
         } catch (IOException e) {
-            e.printStackTrace();
-
+            logException(e);
         }
     }
 
-    public Downloader(){
-        LOG.setUseParentHandlers(false);
-        LOG.setLevel(Level.ALL);
-
-        Handler consoleHandler = new ConsoleHandler();
-        consoleHandler.setFormatter(new LogFormatter());
-
-        LOG.addHandler(consoleHandler);
-    }
-
-    /**
-     * Used to specify where to log Logger messages
-     * @param logFilePath path to file with log messages
-     */
-    public void setFileLogHandler(String logFilePath){
-        Handler fileHandler = null;
-        try {
-            fileHandler = new FileHandler("",true);
-            fileHandler.setLevel(Level.ALL);
-            fileHandler.setFormatter(new LogFormatter());
-            LOG.addHandler(fileHandler);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
 
     /**
      * Method writes all generated (from a template) URLs to a given file
      * @param path path to a file where URLs gonna be written
      */
     public  void writeURLsToFile(String path){
-        File outFile = new File(path);
+        File fileWithGeneratedURLs = new File(path);
         StringBuilder sb = new StringBuilder("");
 
-        for(String url : getVideoURLsBasedOnVideoNumber()){
+        for(String url : generateVideosURLsBasedOnTheirVideoNumber()){
             sb.append(url + "\n");
         }
 
-        try (PrintWriter pw = new PrintWriter(outFile)) {
+        try (PrintWriter pw = new PrintWriter(fileWithGeneratedURLs)) {
             pw.write(sb.toString());
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            logException(e);
         }
+        LOG.info("Generated urls are now in " + path + " file");
     }
 
     /**
      * Method passes current video number to a template and generates video urls
      * @return list of strings with video urls
      */
-    public  List<String> getVideoURLsBasedOnVideoNumber(){
+    public  List<String> generateVideosURLsBasedOnTheirVideoNumber(){
         List<String> videoStrURLsList = new LinkedList<>();
 
-        for(int i = 0 ; i < VIDEO_COUNT; i++){
-            String currentURL = String.format(URL_TEMPLATE,prepareNumberString(i));
+        for(int i = 0; i < numberOfAllVideos +1; i++){
+            String currentURL = String.format(urlTemplate,prepareNumberString(i));
             videoStrURLsList.add(currentURL);
         }
 
@@ -135,11 +155,62 @@ public class Downloader {
                 urls.add(new URL(urlStr));
             }
         } catch (MalformedURLException e) {
-            e.printStackTrace();
+            logException(e);
         }
 
         return urls;
     }
+
+    /**
+     * Method downloads video from given URL and saves it under location and name defined in templates
+     * @param videoUrl  URL from which video gonna be downloaded
+     */
+    public  void downloadVideoFromURLUsingStreamUsingTemplate(URL videoUrl){
+        try {
+            LOG.entering(Downloader.class.getSimpleName(),"downloadVideoFromURLUsingStreamUsingTemplate",videoUrl);
+
+            String fileName = String.format(downloadedFileTemplate,prepareNumberString(currentlyDownloadingVideoNumber));
+            String filePathStr = downloadFolderLocation + "\\" + fileName ;
+            currentlyDownloadingVideoNumber++;
+
+            LOG.info("File gonna be saved in : " + filePathStr);
+
+            File outputFile = new File(downloadFolderLocation,fileName);
+            ReadableByteChannel rbc = Channels.newChannel(videoUrl.openStream());
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+            LOG.info("Downloading finished");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void downloadVideoFromURLUsingStreamAtGivenLocation(String url,String downloadedFileName,
+                                                               String downloadFolderLocation){
+        try {
+
+            String filePathStr = downloadFolderLocation + "\\" + downloadedFileName ;
+
+            LOG.info("File gonna be saved in : " + filePathStr);
+
+            File outputFile = new File(downloadFolderLocation,downloadedFileName);
+            ReadableByteChannel rbc = Channels.newChannel((new URL(url)).openStream());
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+            LOG.info("Downloading finished");
+
+
+        } catch (IOException e) {
+            logException(e);
+        }
+    }
+
+
 
     /**
      * Method downloads video from given URL and saves it under location and name defined in templates
@@ -154,12 +225,12 @@ public class Downloader {
             connection.connect();
 
             LOG.info("Connection established");
-            String fileName = String.format(FILENAME_TEMPLATE,prepareNumberString(VIDEO_NUMBER));
-            String filePathStr = DOWNLOAD_FOLDER_LOCATION + "\\" + fileName ;
-            VIDEO_NUMBER++;
+            String fileName = String.format(downloadedFileTemplate,prepareNumberString(currentlyDownloadingVideoNumber));
+            String filePathStr = downloadFolderLocation + "\\" + fileName ;
+            currentlyDownloadingVideoNumber++;
             LOG.info("File gonna be saved in : " + filePathStr);
 
-            File outputFile = new File(DOWNLOAD_FOLDER_LOCATION,fileName);
+            File outputFile = new File(downloadFolderLocation,fileName);
             FileOutputStream fos = new FileOutputStream(outputFile);
 
             int status = connection.getResponseCode();
@@ -180,63 +251,10 @@ public class Downloader {
             LOG.info("Downloading finished");
 
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            logException(e);
         }
 
-
-    }
-
-    /**
-     * Method downloads video from given URL and saves it under location and name defined in templates
-     * @param videoUrl  URL from which video gonna be downloaded
-     */
-    public  void downloadVideoFromURLUsingStream(URL videoUrl){
-        try {
-            LOG.entering(Downloader.class.getSimpleName(),"downloadVideoFromURLUsingStream",videoUrl);
-            String fileName = String.format(FILENAME_TEMPLATE,prepareNumberString(VIDEO_NUMBER));
-            String filePathStr = DOWNLOAD_FOLDER_LOCATION + "\\" + fileName ;
-            VIDEO_NUMBER++;
-            LOG.info("File gonna be saved in : " + filePathStr);
-
-            File outputFile = new File(DOWNLOAD_FOLDER_LOCATION,fileName);
-            ReadableByteChannel rbc = Channels.newChannel(videoUrl.openStream());
-            FileOutputStream fos = new FileOutputStream(outputFile);
-            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-
-            LOG.info("Downloading finished");
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    /**
-     * Method downloads video from given URL and saves it under location and name defined in templates
-     * @param videoUrl  URL from which video gonna be downloaded
-     * @param videoNumber  downloaded video filename number
-     */
-    public  void downloadVideoFromURLUsingStream(URL videoUrl,int videoNumber){
-        try {
-            LOG.entering(Downloader.class.getSimpleName(),"downloadVideoFromURLUsingStream",videoUrl);
-            String fileName = String.format(FILENAME_TEMPLATE,prepareNumberString(videoNumber));
-            String filePathStr = DOWNLOAD_FOLDER_LOCATION + "\\" + fileName ;
-            VIDEO_NUMBER++;
-            LOG.info("File gonna be saved in : " + filePathStr);
-
-            File outputFile = new File(DOWNLOAD_FOLDER_LOCATION,fileName);
-            ReadableByteChannel rbc = Channels.newChannel(videoUrl.openStream());
-            FileOutputStream fos = new FileOutputStream(outputFile);
-            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-
-            LOG.info("Downloading finished");
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 
--- src/main/java/utils/LogFormatter.java
+++ src/main/java/utils/LogFormatter.java
@@ -7,7 +7,7 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 /**
- * Class used to format a Loggers messages
+ * Class used to format Logger messages
  * @author Aleksander Kołata
  */
 public class LogFormatter
@@ -18,13 +18,14 @@ extends Formatter{
         Level   level = record.getLevel();
         String  message = record.getMessage(),
                 loggerName = record.getLoggerName(),
-                outputMessage = "";
-        long miliseconds = record.getMillis();
-        Date logDate = new Date(miliseconds);
+                outputMessage;
+
+        long milliseconds = record.getMillis();
+        Date logDate = new Date(milliseconds);
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss dd-MM-YYYY");
         String dateStr = sdf.format(logDate);
 
-        outputMessage = dateStr + "\t" + loggerName + "\n" + level + "\t" + message;
+        outputMessage = dateStr + "\t" + loggerName + "\n" + level + "\t" + message + "\n";
 
         return outputMessage;
     }
